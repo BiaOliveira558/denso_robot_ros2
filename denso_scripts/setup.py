a8963eb41dc7638d365ecfce6924634e355a5d8f
BiaOliveira558 <beatrizoliveirabarros255@gmail.com>
2026-03-27 15:12:51 -0300

    criação de pacote para scripts próprios

---
+++ denso_scripts/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'denso_scripts'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+    'console_scripts': [
+        'move_joints = denso_scripts.move_joints:main',
+        'move_xyz    = denso_scripts.move_xyz:main',
+        'main        = denso_scripts.main:main', 
+    ],
+},
+)
